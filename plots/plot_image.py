ff0e7cafb0f2c44b4735c05da4a7ad11f1d9423e
thibault <thibault.latrace@gmail.com>
2021-05-17 21:39:31 +0200

    plot 4 paintings side by side

--- plots/plot_image.py
+++ plots/plot_image.py
@@ -1,13 +1,18 @@
+from pathlib import Path
+
 import matplotlib.pyplot as plt
 
-image_path = "C:/Users/thiba/OneDrive/Documents/dev_projects/painting-style-transfer/archive/training/training/Cezanne/215457.jpg"
+image_paths = [
+    f"C:/Users/thiba/OneDrive/Documents/dev_projects/painting-style-transfer/ressources/training/training/Cezanne/{image_id}.jpg"
+    for image_id in [215457, 215458, 215459, 215460]
+]
 
 
 def plot_image():
-    image = plt.imread(fpath=image_path)
-    fig, ax = plt.subplots()
-    ax.imshow(image)
+    images = [plt.imread(image_path) for image_path in image_paths]
+    fig, ax = plt.subplots(nrows=2, ncols=2)
+    ax[0, 0].imshow(images[0])
+    ax[0, 1].imshow(images[1])
+    ax[1, 0].imshow(images[2])
+    ax[1, 1].imshow(images[3])
     plt.show()
-
-
-plot_image()
