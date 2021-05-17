962d72347c6489937d42e89af086a6a967030f36
thibault <thibault.latrace@gmail.com>
2021-05-17 21:40:04 +0200

    init images utilities

---
+++ utils/image_processing.py
@@ -0,0 +1,12 @@
+from PIL import Image
+
+image_path = "C:/Users/thiba/OneDrive/Documents/dev_projects/painting-style-transfer/ressources/training/training/Cezanne/215457.jpg"
+
+
+def get_image_dimensions(image_path):
+    image = Image.open(image_path)
+    return image.size, image.getbands()
+
+
+dimensions = get_image_dimensions(image_path=image_path)
+print(dimensions)
